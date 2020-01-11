697887a3c0e8154ef1e7d69cc18933d0c89e59c0
Jack Tilley <47064848+Jack-Tilley@users.noreply.github.com>
2020-01-11 13:49:20 -0500

    revised both files to display general path
    no longer used /user/.../

--- ccScraperExample.py
+++ ccScraperExample.py
@@ -1,6 +1,18 @@
+# Jack Tilley
+# January 2020
+# Example file on how to use ccScraper.py
+
 from ccScraper import ClosedCaptionCollector
-url = "https://www.youtube.com/watch?v=k6U-i4gXkLM"
-path = "/Users/Tilley/Downloads/chromedriver"
-cc = ClosedCaptionCollector(url, path)
-cc.create_transcript()
-cc.print_transcript()
+
+url = "https://www.youtube.com/watch?v=k6U-i4gXkLM" # this is the youtube url that you want to get ccs from
+
+path = "path/to/your/chromedriver" # to install chromedriver or with help setting up selenium, follow this link:
+# https://stackoverflow.com/questions/42478591/python-selenium-chrome-webdriver
+
+cc = ClosedCaptionCollector(url, path) # creates a instance of a closed caption transcipt object
+
+cc.create_transcript() # does the grunt work of gathering the closed captions
+
+cc.print_transcript() # prints transcript in human readable form
+
+print(cc.get_transcript()) # prints transcript in list form
